0d0fa046be2f4fc9bde39c3ba5f6aa4d1079090e
ahborkhuja <axbor.yodgorov@gmail.com>
2023-03-29 15:44:05 +0000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart"
+    }
+  }
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(51,213,153)",
+      backgroundColor: "rgba(255,255,255,1)"
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(51,213,153)",
+      backgroundColor: "rgba(255, 255, 255, 1)"
+    }
+  ]
+};
+
+export function App() {
+  return <Line options={options} data={data} />;
+}
